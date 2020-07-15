67e52e4c01687b963340d97d9257fb0f258fac8a
MrOlolo <MrOlolo@users.noreply.github.com>
2020-07-15 22:36:44 +0300

    Update FlutterPDFView.m
    change background color for ios

--- ios/Classes/FlutterPDFView.m
+++ ios/Classes/FlutterPDFView.m
@@ -78,7 +78,7 @@
                 _pdfView.autoresizesSubviews = YES;
                 _pdfView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
                 
-                _pdfView.backgroundColor = [UIColor colorWithWhite:0.95 alpha:1.0];
+                _pdfView.backgroundColor = [UIColor colorWithWhite:1.0 alpha:1.0];
                 BOOL swipeHorizontal = [args[@"swipeHorizontal"] boolValue];
                 if (swipeHorizontal) {
                     _pdfView.displayDirection = kPDFDisplayDirectionHorizontal;
